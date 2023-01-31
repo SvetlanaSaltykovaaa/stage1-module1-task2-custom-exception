5a959323ce97ece2cc736e7460c4046c3d1ced7f
Svetlana <libanonair@gmail.com>
2023-01-31 12:44:30 +0300

    solved

---
+++ src/main/java/com/epam/mjc/NoStudentIDFoundException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+public class NoStudentIDFoundException extends IllegalArgumentException{
+    public NoStudentIDFoundException(String message) {
+        super(message);
+    }
+
+    }
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,26 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws NoStudentIDFoundException {
+    if (Student.getValueOf(studentID) == null) {
+      throw new NoStudentIDFoundException("Could not find student with ID " + studentID);
+    } else {
+      return Student.getValueOf(studentID);
+    }
+
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws NoStudentIDFoundException {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
+      try {
+        Student student = manager.find(1000);
+        System.out.println("Student name " + student.getName());
 
+      } catch (NoStudentIDFoundException e) {
+        System.err.print(e);
+      }
+    }
   }
 }
